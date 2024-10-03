760b357f18e9f00dfc567894ef515aa72e76a434
GONZALEZ HIDALGO <hpamegh77@gmail.com>
2024-10-02 21:11:41 -0600

    Pruebas

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Hilos_Basico
@@ -10,6 +11,30 @@ namespace Hilos_Basico
     {
         static void Main(string[] args)
         {
+            Thread hilo1 = new Thread(funcionHilo);
+
+            Thread hilo2 = new Thread(funcionHilo);
+
+            hilo1.Start();
+            hilo2.Start();
+        }
+        
+        public static void funcionHilo()
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                Console.WriteLine("Hilo Principal "+ i);
+                Thread.Sleep(500);
+            }  
+        }
+         
+        public static void funcionHilo2()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Hilo Principal " + i);
+                Thread.Sleep(500);
+            }
         }
     }
 }
